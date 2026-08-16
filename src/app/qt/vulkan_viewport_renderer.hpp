8cbe3ee7be54f0077f39a902432dc2d81600723e
kacerato <79872194+kacerato@users.noreply.github.com>
2026-08-16 13:38:01 -0300

    add native Vulkan gizmo presentation contract

--- src/app/qt/vulkan_viewport_renderer.hpp
+++ src/app/qt/vulkan_viewport_renderer.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "mobile3d/editor/transform_gizmo.hpp"
 #include "mobile3d/render/render_snapshot.hpp"
 #include "mobile3d/render/viewport_camera.hpp"
 
@@ -12,12 +13,24 @@
 
 class QQuickWindow;
 
+struct VulkanGizmoPresentation final {
+    bool visible{false};
+    m3d::TransformTool tool{m3d::TransformTool::Translate};
+    m3d::TransformSpace space{m3d::TransformSpace::Global};
+    m3d::PivotMode pivotMode{m3d::PivotMode::Median};
+    m3d::Vec3 pivotWorld{};
+    m3d::GizmoBasis basis{};
+    float worldSize{1.0F};
+    std::optional<m3d::TransformConstraint> activeConstraint;
+};
+
 struct VulkanRecordStats final {
     bool recorded{false};
     bool needsAnotherFrame{false};
     bool pipelineCacheLoaded{false};
     std::uint32_t meshDraws{0};
     std::uint32_t outlineDraws{0};
+    std::uint32_t gizmoDraws{0};
 };
 
 struct VulkanPickResult final {
@@ -37,7 +50,7 @@ public:
     VulkanViewportRenderer& operator=(const VulkanViewportRenderer&) = delete;
 
     void synchronize(QRect viewportPixels, m3d::RenderSceneSnapshot snapshot,
-                     m3d::Mat4 viewProjection);
+                     m3d::Mat4 viewProjection, VulkanGizmoPresentation gizmo);
     void requestPick(QPoint viewportPixel);
     [[nodiscard]] VulkanPickResult recordPicking(QQuickWindow* window);
     [[nodiscard]] VulkanRecordStats record(QQuickWindow* window);
@@ -48,5 +61,6 @@ private:
     QRect viewportPixels_;
     m3d::RenderSceneSnapshot snapshot_;
     m3d::Mat4 viewProjection_{};
+    VulkanGizmoPresentation gizmo_{};
     std::optional<QPoint> pendingPickPixel_;
 };
